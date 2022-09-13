919ea466b5ab8ef184d270fafd22015257bf9cab
3salaz <3salaz.dev@gmail.com>
2022-09-13 14:52:36 -0700

    initial start

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Navbar = () => {
+    <nav className='w-full bg-slate-300 flex align-items justify-center'>
+        Some
+    </nav>
+}
+
+export default Navbar;
--- webpack.config.js
+++ webpack.config.js
@@ -2,54 +2,54 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'production',
-    entry: path.join(__dirname, "src", "index.js"),
-    devtool: "source-map",
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'bundle.js',
-      publicPath: '/'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          enforce: 'pre',
-          use: ['source-map-loader'],
-        },
-        {
-          test: /\.?js$/,
-          include: path.resolve(__dirname, 'src'),
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env','@babel/preset-react'],
-            },
+  mode: 'production',
+  entry: path.join(__dirname, "src", "index.js"),
+  devtool: "source-map",
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
+      },
+      {
+        test: /\.?js$/,
+        include: path.resolve(__dirname, 'src'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env','@babel/preset-react'],
           },
         },
-        {
-          test: /\.css$/i,
-          include: path.resolve(__dirname, 'src'),
-          use: ['style-loader', 'css-loader', 'postcss-loader'],
-        },
-        {
-          test: /\.(png|jp(e*)g|svg|gif)$/,
-          use: ['file-loader'],
-        },
-        {
-          test: /\.svg$/,
-          use: ['@svgr/webpack'],
-        }
-      ],
-    },
-    devServer: {
-      static: path.join(__dirname, 'dist/'),
-      port: 3000,
-      historyApiFallback: true,
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: path.join(__dirname, "src", "index.html"),
-      }),
-    ]
-  };
+      },
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, 'src'),
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      }
+    ],
+  },
+  devServer: {
+    static: path.join(__dirname, 'dist/'),
+    port: 3000,
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+  ]
+};
